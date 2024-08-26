37776f57f14e11d0610edda23ce33f8a641cc5b8
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-26 11:57:10 +0200

    Update __manifest__.py

--- letsencrypt/__manifest__.py
+++ letsencrypt/__manifest__.py
@@ -23,9 +23,8 @@
 
 {
     'name': 'Server Tools: Let''s Encrypt',
-    # "version": "12.0.1.0.0",
-    'version': '17.0.1.0.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '1.0.0',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Request SSL certificates from letsencrypt.org',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
