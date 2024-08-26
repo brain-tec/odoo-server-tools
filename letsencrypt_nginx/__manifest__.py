ef49015ae8da75961acbff390f6a6bbfa2df18b5
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-26 11:56:23 +0200

    Update __manifest__.py

--- letsencrypt_nginx/__manifest__.py
+++ letsencrypt_nginx/__manifest__.py
@@ -22,8 +22,8 @@
 
 {
     'name': 'Server Tools: Let''s Encrypt Nginx',
-    'version': '17.0.1.0.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '1.0.0',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Create nginx configs for SSL.',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
